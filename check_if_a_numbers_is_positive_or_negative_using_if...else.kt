f70a725428de792410af89b9bde336ded95838cc
Marcos Vinicius Lima <marcoslimaraimundo@gmail.com>
2019-11-21 22:21:35 -0300

    Create check_if_a_numbers_is_positive_or_negative_using_if...else.kt

---
+++ check_if_a_numbers_is_positive_or_negative_using_if...else.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    val number = 12.3
+    if (number < 0.0)
+        println("$number is a negative number.")
+    else if (number > 0.0)
+        println("$number is a positive number.")
+    else
+        println("$number is 0.")
+}
