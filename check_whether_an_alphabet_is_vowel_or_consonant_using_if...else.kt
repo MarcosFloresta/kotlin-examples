0a925e1673e82c447252803258d022fc626a6709
Marcos Vinicius Lima <marcoslimaraimundo@gmail.com>
2019-11-21 22:21:29 -0300

    Create check_whether_an_alphabet_is_vowel_or_consonant_using_if...else.kt

---
+++ check_whether_an_alphabet_is_vowel_or_consonant_using_if...else.kt
@@ -0,0 +1,5 @@
+fun main(args: Array<String>) {
+    val ch = 'i'
+    val vowelConsonant = if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') "vowel" else "consonant"
+    println("$ch is $vowelConsonant")
+}
