4d4a8843409947388bc6114d5862651e148946e2
Marcos Vinicius Lima <marcoslimaraimundo@gmail.com>
2019-11-23 13:23:47 -0300

    Create generate_multiplication_table_using_while_loop.kt

---
+++ generate_multiplication_table_using_while_loop.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    val num = 9
+    var i = 1
+    
+    while (i <= 10) {
+        val product = num * i
+        println("$num * $i = $product")
+        i++
+    }
+}
