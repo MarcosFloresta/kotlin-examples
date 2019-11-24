a697804aa7b2ae7db575a5a6215a637e4e2ad249
Marcos Vinicius Lima <marcoslimaraimundo@gmail.com>
2019-11-24 13:27:05 -0300

    Create check_if_int_array_contains_a_given_value.kt

---
+++ check_if_int_array_contains_a_given_value.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+    val num = intArrayOf(1, 2, 3, 4, 5)
+    val toFind = 3
+    var found = false
+    for (n in num) {
+        if (n == toFind) {
+            found = true
+            break
+        }
+    }
+    if (found)
+        println("$toFind is found.")
+    else
+        println("$toFind is not found.")
+}
