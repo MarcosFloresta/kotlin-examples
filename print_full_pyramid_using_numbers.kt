04b21afe7041d7f505fa8dcc1417d4766e4e55c8
Marcos Vinicius Lima <marcoslimaraimundo@gmail.com>
2019-11-24 15:03:47 -0300

    Create print_full_pyramid_using_numbers.kt

---
+++ print_full_pyramid_using_numbers.kt
@@ -0,0 +1,26 @@
+fun main(args: Array<String>) {
+    val rows = 5
+    var k = 0
+    var count = 0
+    var count1 = 0
+    for (i in 1..rows) {
+        for (space in 1..rows - i) {
+            print("  ")
+            ++count
+        }
+        while (k != 2 * i - 1) {
+            if (count <= rows - 1) {
+                print((i + k).toString() + " ")
+                ++count
+            } else {
+                ++count1
+                print((i + k - 2 * count1).toString() + " ")
+            }
+            ++k
+        }
+        k = 0
+        count = k
+        count1 = count
+        println()
+    }
+}
