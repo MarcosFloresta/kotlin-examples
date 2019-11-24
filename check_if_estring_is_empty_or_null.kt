e1c7dc73c14b522d98926b35c422417be512680e
Marcos Vinicius Lima <marcoslimaraimundo@gmail.com>
2019-11-24 13:27:09 -0300

    Create check_if_estring_is_empty_or_null.kt

---
+++ check_if_estring_is_empty_or_null.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+    val str1: String? = null
+    val str2 = ""
+    if (isNullOrEmpty(str1))
+        println("str1 is null or empty.")
+    else
+        println("str1 is not null or empty.")
+    if (isNullOrEmpty(str2))
+        println("str2 is null or empty.")
+    else
+        println("str2 is not null or empty.")
+}
+fun isNullOrEmpty(str: String?): Boolean {
+    if (str != null && !str.isEmpty())
+        return false
+    return true
+}
